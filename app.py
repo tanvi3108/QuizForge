b3858fdd2016c28f9b8f7e8b988919ff94938445
tanvi3108 <haldauntanvi@gmail.com>
2026-04-19 22:47:51 +0530

    fixed render port

--- app.py
+++ app.py
@@ -224,5 +224,8 @@ def delete_flashcard_route(fc_id):
     delete_flashcard(fc_id)
     return redirect(url_for("history"))
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
